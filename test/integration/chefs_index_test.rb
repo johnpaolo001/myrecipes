c7dbea6ac1d8432a8588c2911db8f2d268377bc2
John Paolo Paulino <paolo716@gmail.com>
2018-06-21 21:54:40 +0800

    Add listing and pagination

--- test/integration/chefs_index_test.rb
+++ test/integration/chefs_index_test.rb
@@ -5,15 +5,15 @@ class ChefsIndexTest < ActionDispatch::IntegrationTest
   def setup
     @chef = Chef.create!(chefname: "homer", email: "homer@example.com",
                         password: "password", password_confirmation: "password")
-  end
-
-  test "should get chefs index" do
-    get chefs_path
-    assert_response :success
+    @chef2 = Chef.create!(chefname: "john", email: "john@example.com",
+                        password: "password", password_confirmation: "password")
   end
 
   test "should get chefs listing" do
     get chefs_path
+    assert_template 'chefs/index'
     assert_select "a[href=?]", chef_path(@chef), text: @chef.chefname.capitalize
+    assert_select "a[href=?]", chef_path(@chef2), text: @chef2.chefname.capitalize
   end
+
 end
