1ae208bd249c2e436b7609de11832d3712fdc37a
John Paolo Paulino <paolo716@gmail.com>
2018-06-24 21:32:44 +0800

    Add admin authentication and test

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class ChefsEditTest < ActionDispatch::IntegrationTest
   def setup
-    @chef = Chef.create!(chefname: "homer", email: "homer@example.com",
+    @chef = Chef.create!(chefname: "homer1", email: "homer1@example.com",
                         password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "john", email: "john@example.com",
+                        password: "password", password_confirmation: "password")
+    @admin_user = Chef.create!(chefname: "john1", email: "john1@example.com",
+                        password: "password", password_confirmation: "password", admin: true)
   end
 
   test "reject an invalid edit" do
@@ -26,5 +30,29 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     @chef.reload
     assert_match "homer", @chef.chefname
     assert_match "homer@example.com", @chef.email
+  end
+
+  test "accept edit attempt by admin user" do
+    sign_in_as(@admin_user, "password")
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: "homer1", email: "homer1@example.com"} }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "homer1", @chef.chefname
+    assert_match "homer1@example.com", @chef.email
+  end
+
+  test "redirect edit attempt by anoter non-admin user" do
+    sign_in_as(@chef2, "password")
+    updated_name = "joe"
+    updated_email = "joe@example.com"
+    patch chef_path(@chef), params: { chef: { chefname: updated_name, email: updated_email } }
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "homer1", @chef.chefname
+    assert_match "homer1@example.com", @chef.email
   end
 end
